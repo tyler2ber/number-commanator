c903a73dc7dcc7aa3051b7e607cd5f8f0604ee02
Tyler <tyler2ber@gmail.com>
2026-08-13 17:57:30 -0400

    Improved implementation using text_commanated instead of dictionary

--- src/main.py
+++ src/main.py
@@ -1,47 +1,40 @@
 
-print(f"NUMBER-COMMONATOR\n")
-numbers_to_commanate = {}
+print(f"NUMBER-COMMONATOR")
 
-# GET THE numbers_to_commanate
+text_commanated = ""
 with open("original/text.txt", "r") as file:
-    for line in file:
 
-        i_start = -1
-        for index, char in enumerate(line):
-            if char.isdigit():
-                if i_start == -1:
-                    # start new number
-                    i_start = index
-            else:
-                if i_start != -1:
+    text = f"{file.read()} "
 
-                    # end new number
-                    number = line[i_start:index]
-                    i_start = -1
+    digit_indexend = -1
+    for (index, char) in enumerate(text):
+        if not char.isdigit():
+            text_commanated += char # ADD CHAR
+        else:
+            # build entire digitword w/ commas
+            if digit_indexend == -1 or digit_indexend < index:
+                # get digitword
+                digitword = ""
+                digit_index = index
+                digit_char = char
+                while digit_char.isdigit() and digit_index+1 <= (len(text)-1):
+                    digitword += digit_char
+                    digit_index += 1
+                    digit_char = text[digit_index]
+                digit_indexend = digit_index
+                # commonate digitword
+                digitword_backwards = ""
+                i_counter = 0
+                for i in range(len(digitword)-1, -1, -1):
+                    digitword_backwards += f"{digitword[i]}" # the char
+                    i_counter += 1
+                    if i_counter >= 3 and i != 0:
+                        digitword_backwards += "," # the comma
+                        i_counter = 0
+                digitword = "".join(reversed(digitword_backwards))
+                text_commanated += digitword # ADD DIGITWORD
+                print(f"✓ found {digitword}")
 
-                    # add commas
-                    print(f"====> FOUND: {number}")
-                    numberfixed_backwards = ""
-                    d_counter = 0
-                    for d in range(len(number)-1, -1, -1):
-                        numberfixed_backwards += number[d]
-                        d_counter += 1
-                        if d_counter >= 3 and d != 0:
-                            numberfixed_backwards += ","
-                            d_counter = 0
-                    numberfixed = "".join(reversed(numberfixed_backwards))
-                    numbers_to_commanate[number] = numberfixed
-                    print(numberfixed)
-
-# USE THE numbers_to_commanate
-filecontent = ""
-with open("original/text.txt", "r") as file:
-    filecontent = file.read()
-text = ""
-for item in filecontent.split(" "):
-    if item in numbers_to_commanate:
-        item = numbers_to_commanate[item]
-    text += f"{item} "
 with open("edited/text.txt", "w") as file:
-    file.write(text)
-print("\nREPLACED TEXT!")
+    file.write(text_commanated)
+    print(f"SAVED TO: edited/text.txt")
